d488769fc48c81b4b88ecd08fb755283e5c207dd
Hugo s <hugo.sjoholm@elev.ga.ntig.se>
2023-01-25 10:02:59 +0100

    Added basic http requests
    Co-Authored-By: Painini <72918879+Painini@users.noreply.github.com>

---
+++ app/src/index.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose')
+const express = require('express');
+const app = express();
+
+
+
+//const db = require('./persistence');
+
+
+
+
+app.use(express.json());
+app.use(express.static(__dirname + '/static'));
+
+app.listen(3000)
+
+//Show all database entries
+app.get('/', (req, res) => {
+    res.json({message: "Here are all database entries "})
+})
+
+//Show one database entry
+app.get('/:id', (req, res) => {
+    res.json({message: `Here is the database entry for id: ${req.params.id}`})
+})
+
+//Update one database entry
+app.patch('/update:id', (req, res) => {
+    
+})
+//Delete one database entry 
+app.delete ('/delete:id', (req, res) => {
+
+})
+
+
+
+
+
+/*db.init().then(() => {
+    app.listen(3000, () => console.log('Listening on port 3000'));
+}).catch((err) => {
+    console.error(err);
+    process.exit(1);
+});*/
+
+
+
+/*const gracefulShutdown = () => {
+    db.teardown()
+        .catch(() => {})
+        .then(() => process.exit());
+};
+
+
+process.on('SIGINT', gracefulShutdown);
+process.on('SIGTERM', gracefulShutdown);
+process.on('SIGUSR2', gracefulShutdown); // Sent by nodemon*/
